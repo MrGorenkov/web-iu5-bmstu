ea340ba37c6297ef76ac86a0b7b8357e75af0e2d
MrGorenkov <110352407+MrGorenkov@users.noreply.github.com>
2024-10-23 18:02:28 +0300

    Add files via upload

--- backend/server_config/urls.py
+++ backend/server_config/urls.py
@@ -4,9 +4,7 @@ from artwork import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.paintings_list, name='paintings_list'),
-    path('search/', views.paintings_list, name='search'),
+    path('', views.paintings_list, name='paintings_list'),  # объединенный маршрут для списка и поиска
     path('painting/<int:id>/', views.painting_detail, name='painting_detail'),
-    path('order/', views.view_order, name='order_summary'), 
+    path('order/<int:order_id>/', views.view_order, name='order_summary'), 
 ]
-
